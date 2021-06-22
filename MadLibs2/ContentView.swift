c1a7549f923cea8727cf6d8addb3d30192dd11ac
Tim Randall <trtimrandall@gmail.com>
2021-06-22 23:22:43 +1000

    Initial Commit

---
+++ MadLibs2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MadLibs2
+//
+//  Created by Tim Randall on 22/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MadLibs2/MadLibs2App.swift
@@ -0,0 +1,17 @@
+//
+//  MadLibs2App.swift
+//  MadLibs2
+//
+//  Created by Tim Randall on 22/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MadLibs2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
